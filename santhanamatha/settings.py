34319dae153228bbc2234e3c1544c84c721c62b7
dinaharan02 <dinahathul02@gmail.com>
2024-03-04 19:29:22 +0530

    media root

--- santhanamatha/settings.py
+++ santhanamatha/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'santhanamatha.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse("postgres://churchdb_rje4_user:kL2PiBXL7jeDZsaS5uhPhXpTbXmKIceh@dpg"
+                                     "-cngavv6n7f5s73fe75k0-a.oregon-postgres.render.com/churchdb_rje4")
 }
-
-DATABASES["default"] = dj_database_url.parse("postgres://churchdb_rje4_user:kL2PiBXL7jeDZsaS5uhPhXpTbXmKIceh@dpg-cngavv6n7f5s73fe75k0-a.oregon-postgres.render.com/churchdb_rje4")
 
 
 # Password validation
@@ -120,13 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
+
+# LIVE SERVER
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
