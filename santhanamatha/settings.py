39e3aee36f2a5b52121db307f3ef105b747bb458
dinaharan02 <dinahathul02@gmail.com>
2025-02-14 12:48:27 +0530

    debug true

--- santhanamatha/settings.py
+++ santhanamatha/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jd-#+lb$^nw*l22re&d=v$axlk^&ec^zykdxyr@_a(2#+deal%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
